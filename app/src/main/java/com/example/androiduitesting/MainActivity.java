f652252af36de6ab8f1fb32938423c1a1ad264d8
Swayam1129 <swayam2@ualberta.ca>
2025-10-19 19:48:45 -0600

    Lab-07 Done

--- app/src/main/java/com/example/androiduitesting/MainActivity.java
+++ app/src/main/java/com/example/androiduitesting/MainActivity.java
@@ -2,15 +2,20 @@ package com.example.androiduitesting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     // Declare the variables so that you will be able to reference it later.
@@ -30,16 +35,38 @@ public class MainActivity extends AppCompatActivity {
 
         cityList = findViewById(R.id.city_list);
 
-        //String []cities ={"Edmonton", "Vancouver", "Moscow", "Sydney", "Berlin", "Vienna", "Tokyo", "Beijing", "Osaka", "New Delhi"};
+//        String []cities ={"Edmonton", "Vancouver", "Moscow", "Sydney","New Delhi","Mumbai"};
 
         dataList = new ArrayList<>();
 
-        //dataList.addAll(Arrays.asList(cities));
+//        dataList.addAll(Arrays.asList(cities));
 
-        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
 
+
+
+        cityList.setDividerHeight(2);
+        cityAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,dataList){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView text = view.findViewById(android.R.id.text1);
+                text.setTextColor(Color.WHITE);
+                text.setTextSize(20);
+                int padding = (int) (24 * getResources().getDisplayMetrics().density);
+                text.setPadding(padding, padding, padding, padding);
+                view.setBackgroundColor(Color.parseColor("#223C65"));
+                return view;
+            }
+        };
 
         cityList.setAdapter(cityAdapter);
+
+        cityList.setOnItemClickListener((parent, view, position, id) -> {
+            String selectedCity = dataList.get(position);
+            Intent intent = new Intent(MainActivity.this, ShowActivity.class);
+            intent.putExtra("CITY_NAME", selectedCity);
+            startActivity(intent);
+        });
 
         final Button addButton = findViewById(R.id.button_add);
         addButton.setOnClickListener(new View.OnClickListener() {
